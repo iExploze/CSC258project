95de237e07edeae41c13b6952c3e1d2b8930c875
Xian Shen <sx8178919@gmail.com>
2024-11-26 19:31:48 -0500

    The function of drawing scores
    10 functions each for tens place and unit places numbers on the top right corner

---
+++ score drawing.asm
@@ -0,0 +1,554 @@
+.data
+ADDR_DSPL:     .word       0x10008000
+# ...
+
+.text
+# ...
+main:
+    
+game_loop:
+    jal draw_score_unit_place_9
+    j game_loop
+draw_score_tens_place_0:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_tens_place_1:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 124
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_tens_place_2:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_tens_place_3:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_tens_place_4:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_tens_place_5:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_tens_place_6:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_tens_place_7:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_tens_place_8:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_tens_place_9:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 100
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_unit_place_0:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_unit_place_1:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 124
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_unit_place_2:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_unit_place_3:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_unit_place_4:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_unit_place_5:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+
+draw_score_unit_place_6:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_unit_place_7:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_unit_place_8:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
+    
+draw_score_unit_place_9:
+    lw $t0, ADDR_DSPL
+    addi $t2, $t0, 116
+    
+    li $t1, 0xD3D3D3
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 8
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 128
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 120
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    addi $t2, $t2, 4
+    sw $t1 0( $t2 )
+    j game_loop
